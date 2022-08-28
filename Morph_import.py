73ba60777423e154c8bf9088ace9d3fd4f6c6ac5
RoxHat <roxannehattingh@gmail.com>
2022-08-28 17:17:19 +0200

    added correct import function

--- Morph_import.py
+++ Morph_import.py
@@ -7,12 +7,12 @@ h.load_file("stdrun.hoc")
 h.load_file("import3d.hoc")
 h.load_file("stdgui.hoc")
 
-
 #print([x for x in dir(h) if x._contains_('Import')])
-"""
+
 # SWC filename
-filename = "ca1_pyramidal.swc"
-cell = h.Import3d_SWC_read()
+filename = "200929_mbv3_cell1.ASC"
+cell = h.Import3d_Neurolucida3()
+#cell = h.Import3d_MorphML()
 # other options check dir(h)
 cell.input(filename)
 
@@ -23,7 +23,6 @@ i3d = h.Import3d_GUI(cell, 0)
 i3d.instantiate(None)
 
 # Make a figure, just for you to see that it works.
-ps = h.PlotShape(False)  # False tells h.PlotShape not to use NEURON's gui
+ps = h.PlotShape(True)  # False tells h.PlotShape not to use NEURON's gui
 ps.plot(plt)
 plt.show()
-""" 
